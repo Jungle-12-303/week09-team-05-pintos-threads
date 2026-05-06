aaa54f6e400a7371192b92c239e97aef833fef35
minmings111 <minmings111@gmail.com>
2026-05-06 15:34:16 +0900

    feat: fd_entry 구조체 추가

--- pintos/userprog/fd.h
+++ pintos/userprog/fd.h
@@ -1,11 +1,17 @@
 #ifndef USERPROG_FD_H
 #define USERPROG_FD_H
 
+struct file;
+
 enum fd_type {
     FD_STDIN,
     FD_STDOUT,
     FD_FILE,
 };
 
+struct fd_entry {
+    enum fd_type type;
+    struct file* file;
+};
 
 #endif /* USERPROG_FD_H */
